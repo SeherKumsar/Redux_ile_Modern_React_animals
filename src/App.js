192c00fb9f2935c7a7c46544f94bf4a75d428ef8
SeherKumsar <seherkumsar99@outlook.com>
2024-03-02 19:25:56 +0300

    added makeArray funtion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount(count + 1);
-  };
 
-  return (
-    <div>
-      <button onClick={handleClick}>Add Animal</button>
-    <div>
-        Number of animals: {count}
-    </div>
-    </div>
-  );
+    function makeArray() {
+        return [1, 10, 32, 40];
+    }
+
+    // const myArray = makeArray();
+    // const firstElement = myArray[0];
+    // const secondElement = myArray[1];
+
+    const [firstElement, secondElement] = makeArray();
+    console.log(firstElement, secondElement);
+
+
+//   const [count, setCount] = useState(0);
+//   const handleClick = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Add Animal</button>
+//     <div>
+//         Number of animals: {count}
+//     </div>
+//     </div>
+//   );
 }
 
 export default App;
