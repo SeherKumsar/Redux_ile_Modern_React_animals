b5f68c63d4d55fa7fa717ea4983909c915d5366a
SeherKumsar <seherkumsar99@outlook.com>
2024-03-02 19:29:38 +0300

    changed useState on console

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
 
-    function makeArray() {
-        return [1, 10, 32, 40];
-    }
+  console.log(useState(50));
 
-    // const myArray = makeArray();
-    // const firstElement = myArray[0];
-    // const secondElement = myArray[1];
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
-    const [firstElement, secondElement] = makeArray();
-    console.log(firstElement, secondElement);
-
-
-//   const [count, setCount] = useState(0);
-//   const handleClick = () => {
-//     setCount(count + 1);
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleClick}>Add Animal</button>
-//     <div>
-//         Number of animals: {count}
-//     </div>
-//     </div>
-//   );
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Number of animals: {count}</div>
+    </div>
+  );
 }
 
 export default App;
