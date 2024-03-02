2bbdf44b17b3bab4d3c02c50c8240f22c4b99da7
SeherKumsar <seherkumsar99@outlook.com>
2024-03-02 16:29:05 +0300

    added handleclick function to click button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 function App() {
-  // const handleClick = () => {
-  //     console.log('Button was clicked!')
-  // };
+  const handleClick = () => {
+    console.log("Button was clicked!");
+  };
 
   return (
     <div>
-      {/* <button onClick={handleClick}>Add Animal</button> */}
-      {/* <button onClick={function() {
-            console.log('Button was clicked!')
-      }}
-      > */}
-      <button onClick={() => console.log("Button was clicked!")}>
-        Add Animal
-      </button>
+      <button onClick={handleClick}>Add Animal</button>
     </div>
   );
 }
