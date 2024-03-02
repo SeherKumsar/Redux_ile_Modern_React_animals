49b035368511d09faf14c6e184318d7bc8859b52
SeherKumsar <seherkumsar99@outlook.com>
2024-03-02 19:38:56 +0300

    array destruction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { useState } from "react";
 
+// function useState(defaultValue) {
+//     return {
+//         yourState: defaultValue,
+//         yourSetter: () => {},
+//     };
+// }
+
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log(useState(50));
+    // const stateConfig = useState(0);
+    // const count = stateConfig.yourState;
+    // const setCount = stateConfig.yourSetter;
+
+    // const { yourState: count, yourSetter: setCount } = useState(0);
 
   const handleClick = () => {
     setCount(count + 1);
