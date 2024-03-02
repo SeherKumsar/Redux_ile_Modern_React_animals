dafdc0d91e4774e6589fa5d6972c0d185e89797a
SeherKumsar <seherkumsar99@outlook.com>
2024-03-02 21:18:10 +0300

    added getRandomAnimal function

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ function getRandomAnimal() {
 
   return animals[Math.floor(Math.random() * animals.length)];
 }
-console.log(getRandomAnimal());
 
 function App() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
+    // ['cat']
+
+    // ['cat', 'horse']
+
+    // modifies a piece of state
+    // animals.push(getRandomAnimal());
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
+      {/* <div>Number of animals: {count}</div> */}
+      <div>{animals}</div>
     </div>
   );
 }
