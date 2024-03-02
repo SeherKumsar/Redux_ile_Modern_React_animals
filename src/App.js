b53bfebcfefa8be62f385847180e5c8059486b83
SeherKumsar <seherkumsar99@outlook.com>
2024-03-02 16:25:56 +0300

    added callback function to onClick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-function App () {
-    const handleMouseMove = () => {
-        console.log('Button was clicked!')
-    };
+function App() {
+  // const handleClick = () => {
+  //     console.log('Button was clicked!')
+  // };
 
   return (
     <div>
-      <button onMouseMove={handleMouseMove}>Add Animal</button>
+      {/* <button onClick={handleClick}>Add Animal</button> */}
+      {/* <button onClick={function() {
+            console.log('Button was clicked!')
+      }}
+      > */}
+      <button onClick={() => console.log("Button was clicked!")}>
+        Add Animal
+      </button>
     </div>
   );
 }
